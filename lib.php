5ff8fe413ce9bd5fb6a8397218ba80dfcc1df511
Daniel Neis Araujo <danielneis@gmail.com>
2014-08-25 15:56:58 -0300

    adding copyright headers

--- lib.php
+++ lib.php
@@ -1,3 +1,22 @@
 <?php
 
+// Este bloco é parte do Moodle Provas - http://tutoriais.moodle.ufsc.br/provas/
+//
+// O Moodle Provas pode ser utilizado livremente por instituições integradas à
+// UAB - Universidade Aberta do Brasil (http://www.uab.capes.gov.br/), assim como ser
+// modificado para adequação à estrutura destas instituições
+// sobre os termos da "GNU General Public License" como publicada pela
+// "Free Software Foundation".
+
+// You should have received a copy of the GNU General Public License
+// along with this plugin.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains support functions.
+ *
+ * @package    local_exam_authorization
+ * @copyright  2014 onwards Antonio Carlos Mariani (https://moodle.ufsc.br)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
