bc8db69dc296c2fb9211f902751f34c428f75a9e
Antonio Carlos Mariani <a.c.mariani@inf.ufsc.br>
2014-10-11 13:30:34 -0300

    Atualizando versão

--- version.php
+++ version.php
@@ -21,6 +21,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014080500;
-$plugin->requires  = 2013111800; //Moodle 2.6.1
+$plugin->version   = 2014101100;
+$plugin->requires  = 2013111800; //Moodle 2.6
 $plugin->component = 'local_exam_authorization';
