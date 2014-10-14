b7713127bda6037b46ef456e98bbebfadf2aba5d
Daniel Neis Araujo <daniel@setic.ufsc.br>
2014-10-14 14:18:43 -0300

    Adicionando licenças.

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ Há um script em cli/configure_moodle_provas.php que realizar diversas operaçõ
     - remove diversas permissões dos papeis estudante e professor;
     - oculta/desativa diversos módulos e blocos (forum, message, etc)
     - altera diversos parâmetros globais de configuração
+
+Licença
+=======
+
+Você deve ter recebido uma cópia da GNU General Public License
+com este módulo. Caso negativo, visite <http://www.gnu.org/licenses/>.
--- version.php
+++ version.php
@@ -1,21 +1,31 @@
 <?php
-
-// Este bloco é parte do Moodle Provas - http://tutoriais.moodle.ufsc.br/provas/
+// This file is part of Moodle - http://moodle.org/
 //
-// O Moodle Provas pode ser utilizado livremente por instituições integradas à
-// UAB - Universidade Aberta do Brasil (http://www.uab.capes.gov.br/), assim como ser
-// modificado para adequação à estrutura destas instituições
-// sobre os termos da "GNU General Public License" como publicada pela
-// "Free Software Foundation".
-
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
 // You should have received a copy of the GNU General Public License
-// along with this plugin.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Este bloco é parte do Moodle Provas - http://tutoriais.moodle.ufsc.br/provas/
+// Este projeto é financiado pela
+// UAB - Universidade Aberta do Brasil (http://www.uab.capes.gov.br/)
+// e é distribuído sob os termos da "GNU General Public License",
+// como publicada pela "Free Software Foundation".
 
 /**
  * Version details
  *
  * @package    local_exam_authorization
- * @copyright  2014 onwards Antonio Carlos Mariani (https://moodle.ufsc.br)
+ * @author     Antonio Carlos Mariani
+ * @copyright  2010 onwards Universidade Federal de Santa Catarina (http://www.ufsc.br)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
